2199792836de3ee001524367879f4caf2ba6ef38
Dmytro Boichuk <dboichuk@mail.greenriver.edu>
2021-04-23 14:52:17 -0700

    working on project

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+
+public interface GreetingService {
+    String sayGreeting();
+}
